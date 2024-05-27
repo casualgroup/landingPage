46519cf62e691ba70314b1e76c886308b08f8ea4
Juarez José <juarezj.junior@unifacol.edu.br>
2024-05-27 12:36:46 -0300

    arquivos do laravel

---
+++ resources/views/Components/layout.blade.php
@@ -0,0 +1,71 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>{{ $title }}</title>
+  <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+<header>
+  <div class="px-3 py-2 text-bg-dark border-bottom">
+    <div class="container">
+      <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+        <a href="/" class="navbar-brand d-flex align-items-center my-2 my-lg-0 me-lg-auto text-white text-decoration-none">
+          Casual Group
+        </a>
+
+        <ul class="nav col-12 col-lg-auto my-2 justify-content-center my-md-0 text-small">
+          <li>
+            <a href="#" class="nav-link text-white">
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="#" class="nav-link text-white">
+              Tecnologias que usamos
+            </a>
+          </li>
+          <li>
+            <a href="#" class="nav-link text-white">
+              Nossos Projetos
+            </a>
+          </li>
+          <li>
+            <a href="#" class="nav-link text-white">
+              Nossa Equipe
+            </a>
+          </li>
+          <li>
+            <a href="#" class="nav-link text-white">
+              Contato
+            </a>
+          </li>
+          <li>
+            <a href="#" class="nav-link text-white">
+              FAQ
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</header>
+<main>
+  <div>
+    {{ $slot }}
+  </div>
+</main>
+<footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+  <p class="col-md-4 mb-0 text-muted">© Casual Group. 2021</p>
+
+
+  <ul class="nav col-md-4 justify-content-end">
+    <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Instagram</a></li>
+    <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Email</a></li>
+  </ul>
+</footer>
+</body>
+</html>
