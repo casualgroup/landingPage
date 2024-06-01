f71266711ecf9d8c12a7081d54f519943cfdc545
Juarez José <juarezj.junior@unifacol.edu.br>
2024-06-01 13:12:01 -0300

    alinhado

--- resources/views/landing/faq.blade.php
+++ resources/views/landing/faq.blade.php
@@ -3,7 +3,7 @@
     <h1 class="p-4 fw-bold text-center">FAQ</h1>
 
     <div class="p-4 col text-center">
-        <div class="col-12">
+        <div class="col-12 container">
           <x-projectLayout titulo="O que é Casual Group?"
                            description="é uma empresa de tecnologia que fornece soluções de software sobdemanda para seus clientes. Nosso foco principal é desenvolver e fornecer plataformas e ferramentas inovadoras para ajudar empresas"
                           
@@ -12,14 +12,10 @@
       </div>
     </div>
     <div class="p-4 col text-center">
-      <div class="row container text-center">
-        <div class="col-14">
-          <x-projectLayout titulo="Quais serviços a Casual Group oferece?"
-                           description="Dentro do leque de projetos da Casual Group temos: LII Weather, um aplicativo de previsão do tempo para dispositivos moveis, SpaceP, um aplicativo que mostra imagens tiradas pela nasa por data, E uma ferrmaneta que ainda está em desenvolvimento chamada Controle Total, um sistema de gestão para consultorios."
-                           ></x-projectLayout>
-          
-     
-      </div>
+      <div class="container col-12">
+        <x-projectLayout titulo="Quais serviços a Casual Group oferece?"
+                         description="Dentro do leque de projetos da Casual Group temos: LII Weather, um aplicativo de previsão do tempo para dispositivos moveis, SpaceP, um aplicativo que mostra imagens tiradas pela nasa por data, E uma ferrmaneta que ainda está em desenvolvimento chamada Controle Total, um sistema de gestão para consultorios."
+                         ></x-projectLayout>
     </div>
   </div>
   <p class = "text-center">
