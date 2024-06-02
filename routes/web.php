1df4a8ccf187033d7110f6f00630101d010d18c7
CarlosEduardoMarquesdeAlmeida <carlose.maciel@unifacol.edu.br>
2024-06-02 20:21:45 -0300

    Criação da tela de tecnologias

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-  return view('landing.index');
+  return view('landing.tecnologias');
 });
 
 //Route::resource('/series', SeriesController::class)
