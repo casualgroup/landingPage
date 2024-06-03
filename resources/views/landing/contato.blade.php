b6108bbf3409d67e858c807f80634535beea4979
Thiago Borges <thiagoborges637@gmail.com>
2024-06-03 14:46:30 -0300

    Faq e Contato update

--- resources/views/landing/contato.blade.php
+++ resources/views/landing/contato.blade.php
@@ -1,82 +1,82 @@
 <x-layout title="Contato">
   <div class="container">
-    <h1 class="p-4 fw-bold text-center">Contato</h1>
-
-<div style="width: 873px; height: 98px; position: relative">
-  <div style="left: 304px; top: 0px; position: absolute; color: black; font-size: 40px; font-family: Poppins; font-weight: 700; word-wrap: break-word">Contate-nos</div>
-  <div style="width: 873px; height: 47px; padding: 10px; left: 0px; top: 51px; position: absolute; justify-content: center; align-items: center; gap: 10px; display: inline-flex">
-    <div style="justify-content: center; align-items: center; gap: 10px; display: flex">
-      <div style="text-align: center; color: #717171; font-size: 18px; font-family: Poppins; font-weight: 500; word-wrap: break-word">Duvidas, marcações ou suporte? Selecione uma das opções para entrar em contato conosco.</div>
-    </div>
-  </div>
+    <h1 class="p-4 fw-bold text-center">Contate-nos</h1>
+    <div style="text-align:center">
+    <p> Duvidas, marcações ou suporte? Preencha os campos abaixo para entrar em contato conosco.</p>
 </div>
-<div style="width: 292px; height: 48px; position: relative">
-  <div style="width: 292px; height: 0px; left: 0px; top: 48px; position: absolute; background: #8D8D8D; border: 1px #8D8D8D solid"></div>
-  <div style="width: 44.49px; height: 17.88px; left: 0px; top: 0px; position: absolute; color: black; font-size: 12px; font-family: Poppins; font-weight: 500; line-height: 20px; word-wrap: break-word">Email</div>
-</div>
-<div style="width: 329px; height: 83px; position: relative">
-  <div style="width: 329px; height: 0px; left: 0px; top: 83px; position: absolute; background: #8D8D8D; border: 1px #8D8D8D solid"></div>
-  <div style="width: 71px; height: 31px; left: 0px; top: 0px; position: absolute; color: black; font-size: 12px; font-family: Poppins; font-weight: 500; line-height: 20px; word-wrap: break-word">Mensagem</div>
-</div>
-<div style="width: 337px; height: 244px; position: relative">
-  <div style="width: 24px; height: 24px; left: 0px; top: 148px; position: absolute">
-    <div style="width: 16.50px; height: 21px; left: 3.75px; top: 1.50px; position: absolute; background: black"></div>
-    <div style="width: 6px; height: 6px; left: 9px; top: 6.75px; position: absolute"></div>
-  </div>
-  <div style="width: 137px; height: 24px; left: 0px; top: 0px; position: absolute">
-    <div style="width: 24px; height: 24px; left: 0px; top: 0px; position: absolute">
-      <div style="width: 9.01px; height: 9px; left: 12.99px; top: 2px; position: absolute; background: black"></div>
-      <div style="width: 17.81px; height: 17.81px; left: 3px; top: 3.19px; position: absolute; background: black"></div>
-    </div>
-    <div style="left: 49px; top: 0px; position: absolute; color: black; font-size: 16px; font-family: Poppins; font-weight: 400; word-wrap: break-word">3523-9045</div>
-  </div>
-  <div style="width: 316px; height: 24px; left: 0px; top: 74px; position: absolute">
-    <div style="width: 24px; height: 24px; left: 0px; top: 0px; position: absolute">
-      <div style="width: 20px; height: 16px; left: 2px; top: 4px; position: absolute; background: black"></div>
-    </div>
-    <div style="left: 49px; top: 0px; position: absolute; color: black; font-size: 16px; font-family: Poppins; font-weight: 400; text-decoration: underline; word-wrap: break-word">contatocasualgroup@gmail.com</div>
-  </div>
-  <div style="width: 288px; left: 49px; top: 148px; position: absolute; color: black; font-size: 16px; font-family: Poppins; font-weight: 400; word-wrap: break-word">R. Pedro Ribeiro, 85 - Universitário, Vitória de Santo Antão - PE, 55612-285<br/></div>
-</div>
-<div style="width: 337px; height: 301px; position: relative">
-  <div style="width: 24px; height: 24px; left: 0px; top: 148px; position: absolute">
-    <div style="width: 16.50px; height: 21px; left: 3.75px; top: 1.50px; position: absolute; background: black"></div>
-    <div style="width: 6px; height: 6px; left: 9px; top: 6.75px; position: absolute"></div>
-  </div>
-  <div style="width: 137px; height: 24px; left: 0px; top: 0px; position: absolute">
-    <div style="width: 24px; height: 24px; left: 0px; top: 0px; position: absolute">
-      <div style="width: 9.01px; height: 9px; left: 12.99px; top: 2px; position: absolute; background: black"></div>
-      <div style="width: 17.81px; height: 17.81px; left: 3px; top: 3.19px; position: absolute; background: black"></div>
-    </div>
-    <div style="left: 49px; top: 0px; position: absolute; color: black; font-size: 16px; font-family: Poppins; font-weight: 400; word-wrap: break-word">3523-9045</div>
-  </div>
-  <div style="width: 316px; height: 24px; left: 0px; top: 74px; position: absolute">
-    <div style="width: 24px; height: 24px; left: 0px; top: 0px; position: absolute">
-      <div style="width: 20px; height: 16px; left: 2px; top: 4px; position: absolute; background: black"></div>
-    </div>
-    <div style="left: 49px; top: 0px; position: absolute; color: black; font-size: 16px; font-family: Poppins; font-weight: 400; text-decoration: underline; word-wrap: break-word">contatocasualgroup@gmail.com</div>
-  </div>
-  <div style="width: 288px; left: 49px; top: 148px; position: absolute; color: black; font-size: 16px; font-family: Poppins; font-weight: 400; word-wrap: break-word">R. Pedro Ribeiro, 85 - Universitário, Vitória de Santo Antão - PE, 55612-285<br/></div>
-  <div style="width: 288px; left: 49px; top: 253px; position: absolute; color: black; font-size: 16px; font-family: Poppins; font-weight: 400; word-wrap: break-word">@Casualgroup<br/></div>
-</div>
-<div style="width: 292px; height: 109px; position: relative">
-  <div style="width: 292px; height: 109px; left: 0px; top: 0px; position: absolute">
-    <div style="width: 292px; height: 0px; left: 0px; top: 61.88px; position: absolute; border: 1px black solid"></div>
-    <div style="width: 129px; height: 22px; left: 0px; top: 0px; position: absolute; color: black; font-size: 12px; font-family: Poppins; font-weight: 500; line-height: 20px; word-wrap: break-word">Numero de Telefone</div>
-    <div style="width: 144px; height: 23px; left: 0px; top: 28px; position: absolute; color: #B8B8B8; font-size: 14px; font-family: Poppins; font-weight: 500; line-height: 20px; word-wrap: break-word">ex: (00) 0000-0000</div>
-    <div style="width: 208px; height: 23px; left: 0px; top: 86px; position: absolute; color: #B8B8B8; font-size: 14px; font-family: Poppins; font-weight: 500; line-height: 20px; word-wrap: break-word">ex: casualgroup@gmail.com</div>
-  </div>
-  <div style="width: 25.21px; height: 27px; left: 238.43px; top: 54px; position: absolute">
-    <div style="width: 19.21px; height: 26.57px; left: 2.24px; top: 0.28px; position: absolute; background: black"></div>
-    <div style="width: 25.21px; height: 27px; left: 0px; top: 0px; position: absolute"></div>
-  </div>
-</div>
-<div style="width: 32px; height: 30px; position: relative">
-  <div style="width: 32px; height: 30px; left: 0px; top: 0px; position: absolute; background: white; border-radius: 9999px"></div>
-  <div style="width: 16px; height: 15px; left: 8.53px; top: 7px; position: absolute; background: black"></div>
-</div>
-<div style="width: 147px; height: 47px; background: black"></div>
-<div style="color: white; font-size: 12px; font-family: Poppins; font-weight: 500; line-height: 20px; word-wrap: break-word">ENVIAR MENSAGEM</div>
-<div style="width: 366px; opacity: 0.50; color: black; font-size: 13px; font-family: Manrope; font-weight: 500; word-wrap: break-word">*Ao enviar uma mensagem, você concorda com termos e condições impostas pela plataforma de mensagem utilizada</div>
-<div style="width: 383px; height: 0px; transform: rotate(-90.15deg); transform-origin: 0 0; border: 1px black solid"></div>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css" rel="stylesheet">
+<div class="container">
+    <div class="contato shadow-lg mt-n9">
+        <div class="row no-gutters">
+            <div class="col-lg-5 contact-info gradient-brand-color p-5 order-lg-2">
+                <h3 class="color--white mb-5">Informações de Contato</h3>
+    
+                <ul class="contact-info__list list-style--none position-relative z-index-101">
+                    <li class="mb-4 pl-4">
+                        <span class="position-absolute"><i class="fas fa-envelope"></i></span> .... contatocasualgroup@gmail.com
+                    </li>
+                    <li class="mb-4 pl-4">
+                        <span class="position-absolute"><i class="fas fa-phone"></i></span> .... 3523-9045
+                    </li>
+                    <li class="mb-4 pl-4">
+                        <span class="position-absolute"><i class="fas fa-map-marker-alt"></i></span> .... R. Pedro Ribeiro, 85 - Universitario
+                        <br> Vitoria de Santo Antao - PE
+                        <br> 55612-285
+    
+                       
+                    </li>
+                </ul>
+    
+            </div>
+    
+            <div class="col-lg-7 contato p-5 order-lg-1">
+                <form action="#" class="contato form-validate" novalidate="novalidate">
+                    <div class="row">
+                        <div class="col-sm-6 mb-3">
+                            <div class="form-group">
+                                <label class="required-field" for="Nome">Nome</label>
+                                <input type="text" class="form-control" id="Nome" name="Nome" placeholder="Ex: Thiago">
+                            </div>
+                        </div>
+    
+                        <div class="col-sm-6 mb-3">
+                            <div class="form-group">
+                                <label for="Sobrenome">Sobrenome</label>
+                                <input type="text" class="form-control" id="Sobrenome" name="Sobrenome" placeholder="Ex: Banks">
+                            </div>
+                        </div>
+    
+                        <div class="col-sm-6 mb-3">
+                            <div class="form-group">
+                                <label class="required-field" for="email">Email</label>
+                                <input type="text" class="form-control" id="email" name="email" placeholder="Ex: casualGroup@gmail.com">
+                            </div>
+                        </div>
+    
+                        <div class="col-sm-6 mb-3">
+                            <div class="form-group">
+                                <label for="Numero">Numero de Telefone</label>
+                                <input type="tel" class="form-control" id="Numero" name="Numero" placeholder="Ex: (00)0000-0000">
+                            </div>
+                        </div>
+    
+                        <div class="col-sm-12 mb-3">
+                            <div class="form-group">
+                                <label class="required-field" for="Mensagem">Mensagem</label>
+                                <textarea class="form-control" id="Mensagem" name="Mensagem" rows="4" placeholder="Escreva aqui sua mensagem..."></textarea>
+                            </div>
+                        </div>
+    
+                        <div class="col-sm-12 mb-3">
+                            <button type="submit" name="submit" class="btn btn-primary">ENVIAR MENSAGEM</button>
+                           
+                            <p> *Ao enviar uma mensagem, você concorda com termos e condições impostas pela plataforma de mensagem utilizada </p>
+                        </div>
+    
+                    </div>
+                </form>
+            </div>
+    
+        </div>
+    </div></div>
 </x-layout>
