99ca74c28c89a4d4ce3f21fe13c3323c682766fe
AntonioSantiagoN <Reneki2903@outlook.com>
2024-06-02 21:06:40 -0300

    Criação da pagina "Nossa Equipe"

--- resources/views/landing/index.blade.php
+++ resources/views/landing/index.blade.php
@@ -1,5 +1,6 @@
-<x-layout title="título legal">
+<x-layout title="Sobre nos">
   <!-- coloque o título /\ da página aqui  -->
+  
 <!-- crie uma view `php artisan make:view <lading.nomeDaView>` -->
   <!-- faça o código com base no modelo do figma -->
   <!-- o código devera usar bootstrap -->
--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-  return view('landing.index');
+Route::get('sobreNos', function () {
+  return view('landing.sobreNos');
 });
+
 
 //Route::resource('/series', SeriesController::class)
 //  ->except(['show']);
