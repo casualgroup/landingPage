bc83592dd243b34209b8c847a723aca29a94a460
Juarez José <juarezj.junior@unifacol.edu.br>
2024-06-04 20:11:09 -0300

    é isso ai

--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -13,6 +13,7 @@
   <div class="px-3 py-2 text-bg-dark border-bottom fixed-top">
     <div class="container">
       <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-center">
+        <img src="{{asset('img/logoIconWhite.png')}}" alt="Logo Casual Group" width="5%">
         <a href="/" class="navbar-brand d-flex align-items-center my-2 my-lg-0 me-lg-auto text-white text-decoration-none">
           Casual Group
         </a>
